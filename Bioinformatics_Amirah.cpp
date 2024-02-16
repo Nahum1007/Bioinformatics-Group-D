b149c589248df21cb1793d9d02b4f83bbc85c078
Amirah2023 <124348955+Amirah2023@users.noreply.github.com>
2024-02-17 00:09:45 +0100

    Add files via upload

---
+++ Bioinformatics_Amirah.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Name: Amirah Abdulrahman" << std::endl;
+    std::cout << "Email: amirahabdulrahman2018@gmail.com" << std::endl;
+    std::cout << "Slack username: @Amirah_Abdulrahman" << std::endl;
+    std::cout << "Area of interest: Gene therapy" << std::endl;
+
+    return 0;
+}
