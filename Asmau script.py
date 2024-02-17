1051f3fc88f15fb519a24b3a75bac67b2c7135cc
Asmeeim <157611396+Asmeeim@users.noreply.github.com>
2024-02-17 15:11:09 +0100

    Update and rename Asmau script to Asmau script.py

---
+++ Asmau script.py
@@ -0,0 +1,20 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print ('MY INFORMATIONS')
+print('NAME: Muhammad Asmau Ibrahim')
+print('email: asmeeim03@gmail.com')
+print('slack username: @asmeeim')
+print('Area of interest: Genomics & Database Management')
+
+Output:
+        MY INFORMATIONS
+NAME: Muhammad Asmau Ibrahim
+email: asmeeim03@gmail.com
+slack username: @asmeeim
+Area of interest: Genomics & Database Management
