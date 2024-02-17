4ca29a78e253d7bc82a7b268eb0c1685de3077cc
Amirah2023 <124348955+Amirah2023@users.noreply.github.com>
2024-02-17 15:28:43 +0100

    Update Bioinformatics_Amirah.cpp

--- Bioinformatics_Amirah.cpp
+++ Bioinformatics_Amirah.cpp
@@ -4,7 +4,7 @@ int main() {
     std::cout << "Name: Amirah Abdulrahman" << std::endl;
     std::cout << "Email: amirahabdulrahman2018@gmail.com" << std::endl;
     std::cout << "Slack username: @Amirah_Abdulrahman" << std::endl;
-    std::cout << "Area of interest: Gene therapy" << std::endl;
+    std::cout << "Area of interest: data analysis" << std::endl;
 
     return 0;
 }
