c77baca6c35076d46581d0f6c8817031218d9b39
Hauwa03 <160315403+Hauwa03@users.noreply.github.com>
2024-02-17 11:21:47 +0100

    Add files via upload

---
+++ Bioinformatics_Hauwa.java
@@ -0,0 +1,15 @@
+public class BioinformaticsInfo {
+    public static void main(String[] args) {
+        // Information
+        String name = "Hauwa Abubakar Yaro";
+        String slackUsername = "@HauwaYaro";
+        String email = "hauwauabubakaryaro61@gmail.com";
+        String areaOfInterest = "Computational Biology";
+
+        // Print information
+        System.out.println("Name: " + name);
+        System.out.println("Slack Username: " + slackUsername);
+        System.out.println("Email: " + email);
+        System.out.println("Area of Interest: " + areaOfInterest);
+    }
+}
