e3e4efa963cfc008a0d4007b37aa44c46438b04e
Nahum1007 <124349143+Nahum1007@users.noreply.github.com>
2024-02-17 15:22:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-'''
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
-print ('MY INFORMATIONS')
-print('NAME: MUHAMMAD ASMAU IBRAHIM')
-print('asmeeim03@gmail.com')
-print('@asmeeim')
-print('histopathology')
